75dbaea747b9c5aae692cb2525a9a81437602ca2
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-10 19:31:41 +0100

    code added, modiefied

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ imgBox.addEventListener('mousemove', (e) => {
 	const y = e.clientY - e.target.offsetTop;
 
 	console.log(x, y);
+
+	img.style.transformOrigin = `${x}px ${y}px`;
+	img.style.transform = 'scale(2)';
+});
+
+imgBox.addEventListener('mouseleave', () => {
+	img.style.transformOrigin = 'center';
+	img.style.transform = "scale(1)"
 });
