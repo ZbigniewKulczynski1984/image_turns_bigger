4168a8efc841704fbe6b5a501f893217b4c1cd0a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-10 19:18:44 +0100

    modified

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-const img = document.querySelector('img');
+const img = document.querySelector('img')
 
-const zoomImg = (e) => {
-	const x = e.clientX;
-	const y = e.clientY;
+const zoomImg = e => {
+	const x = e.clientX
+	const y = e.clientY
 
-	// console.log(x, y)
+	const imgX = img.offsetLeft
+	const imgY = img.offsetTop
 
-	const imgX = img.offsetLeft;
-	const imgY = img.offsetTop;
+	const newX = (imgX - x) * -1
+	const newY = (imgY - y) * -1
 
-	const newX = (imgX - x) * -1;
-	const newY = (imgY - y) *  -1;
+	img.style.transformOrigin = `${newX}px ${newY}px`
+	img.classList.add('zoom-img')
+}
 
-	// console.log(newX)
+const resetImg = () => {
+	img.classList.remove('zoom-img')
+}
 
-	img.style.transformOrigin = `${newX}px ${newY}px`;
-
-	img.classList.add('zoom-img');
-};
-
-const removeImg = () => {
-	img.classList.remove('zoom-img');
-};
-
-img.addEventListener('mousemove', zoomImg);
-img.addEventListener('mouseout', removeImg);
+img.addEventListener('mousemove', zoomImg)
+img.addEventListener('mouseout', resetImg)
