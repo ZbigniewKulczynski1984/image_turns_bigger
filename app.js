13dd6fd83c34e61c2bd99ce4324579bc534bd650
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-10 19:12:07 +0100

    code added

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const zoomImg = (e) => {
 	const imgX = img.offsetLeft;
 	const imgY = img.offsetTop;
 
+	const newX = (imgX - x) * -1;
+	const newY = (imgY - y) *  -1;
+
+	// console.log(newX)
+
+	img.style.transformOrigin = `${newX}px ${newY}px`;
+
 	img.classList.add('zoom-img');
 };
 
@@ -18,4 +25,3 @@ const removeImg = () => {
 
 img.addEventListener('mousemove', zoomImg);
 img.addEventListener('mouseout', removeImg);
-
